756c4f628ee6e4b3d48ecc841b505c50e7ace85b
modernscape <modernscape@gmail.com>
2025-12-27 21:20:39 +0900

    25-12-27_

--- 25-12-25_three-wasm-sample/main.js
+++ 25-12-25_three-wasm-sample/main.js
@@ -1,10 +1,52 @@
 import * as THREE from "three";
-import PointModule from "./pointcloud";
+import PointModule from "/pointcloud.js";
 
-const COUNT = 1000;
+const COUNT = 100;
 
 PointModule().then((Module) => {
   const ptr = Module._generate_points(COUNT);
 
-  const positions = new Float32Array(Module.HEAPF32.buffer, ptr, COUNT * 3);
+  console.log("Module.HEAPF32"); // OK
+  console.log(Module.HEAPF32); // OK
+
+  //   const positions = new Float32Array(COUNT * 3);
+  //   for (let i = 0; i < COUNT * 3; i++) {
+  //     positions[i] = Module.getValue(ptr + i * 4, "float");
+  //   }
+
+  // ★ここが核心：Wasm メモリをそのまま参照
+  const positions = Module.HEAPF32.subarray(
+    ptr >> 2, // ptr / 4
+    (ptr >> 2) + COUNT * 3
+  );
+
+  console.log(positions.slice(0, 9));
+
+  Module._free_points(ptr);
+
+  // Three.js Geometry
+  const geometry = new THREE.BufferGeometry();
+  geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
+
+  const material = new THREE.PointsMaterial({
+    size: 0.05,
+    color: 0xffffff,
+  });
+
+  const points = new THREE.Points(geometry, material);
+  const scene = new THREE.Scene();
+  scene.add(points);
+
+  const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 100);
+  camera.position.z = 5;
+
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+  }
+  animate();
 });
