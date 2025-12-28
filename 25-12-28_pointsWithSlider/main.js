440733be1acf3f03f24e64c8ffaa735b9f9bfc91
modernscape <modernscape@gmail.com>
2025-12-29 00:36:55 +0900

    25-12-29_

--- 25-12-28_pointsWithSlider/main.js
+++ 25-12-28_pointsWithSlider/main.js
@@ -9,4 +9,22 @@ init();
 
 async function init() {
   wasm = await Module();
+
+  console.log(wasm);
+  // --- Three.js 初期化 ---
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(
+    60,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    100 //
+  );
+  camera.position.z = 3;
+
+  renderer = new THREE.WebGLRenderer({antialias: true});
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  // 初期点群
 }
